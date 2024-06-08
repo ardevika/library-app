2eb816f3b0add121ec9c48e7bcc9d7debe9411d7
devikaar <devikaar6801@gmail.com>
2024-06-08 16:24:16 +0530

    message

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './NavBar'
 
 const AddBook = () => {
+    const[data,setData]=useState(
+        {
+          "Name"  :"",
+          "Description":"",
+          "pulisher":"",
+          "Distributor":""
+
+
+        }
+    )
+    const inputHandler=(event)=>{
+        setData({...data,[event.target.name]: event.target.value})
+    }
+    const readValue=()=>{
+        console.log(data)
+    }
     return (
         <div>
             <NavBar/>
@@ -11,7 +27,7 @@ const AddBook = () => {
                         <div className="row g-3">
                             <div className="col col-12 col-sm-6 col-md-16 col-lg-6 col-xl-6 col-xxl-6">
                                 <label htmlFor="" className="form-label">Name</label>
-                                <input type="text" className="form-control" />
+                                <input type="text" className="form-control" name='Name' value={data.Name} onChange={inputHandler} />
                             </div>
                             <div className="col col-12 col-sm-6 col-md-16 col-lg-6 col-xl-6 col-xxl-6">
                                 <label htmlFor="" className="form-label">year</label>
@@ -23,19 +39,19 @@ const AddBook = () => {
                             </div>
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                 <label htmlFor="" className="form-label">Description</label>
-                                <textarea name="" id="" className="form-control"></textarea>
+                                <textarea name="" id="" className="form-control" name='Description' value={data.Description} onChange={inputHandler}></textarea>
                             </div>
                             <div className="col col-12 col-sm-6 col-md-16 col-lg-6 col-xl-6 col-xxl-6">
                                 <label htmlFor="" className="form-label">Distributor Name</label>
-                                <input type="text" className="form-control" />
+                                <input type="text" className="form-control" name='Distributor' value={data.Distributor} onChange={inputHandler} />
                             </div>
                             <div className="col col-12 col-sm-6 col-md-16 col-lg-6 col-xl-6 col-xxl-6">
                                 <label htmlFor="" className="form-label">pulisher</label>
-                                <input type="text" className="form-control" />
+                                <input type="text" className="form-control" name='pulisher' value={data.pulisher} onChange={inputHandler} />
                             </div>
                             <div className="col col-12 col-sm-6 col-md-16 col-lg-6 col-xl-6 col-xxl-6">
-                                <label htmlFor="" className="form-label">Add</label> 
-                                <button className="btn btn-success">here</button> 
+                                <label htmlFor="" className="form-label"></label> 
+                                <button className="btn btn-success" onClick={readValue}>add</button> 
                             </div>
                         </div>
                     </div>
--- src/components/SearchBook.jsx
+++ src/components/SearchBook.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './NavBar'
 
 const SearchBook = () => {
+    const[data,setData]=useState(
+        {"name":""
+        }
+    )
+    const inputHandler=(event)=>{
+        setData({...data,[event.target.name]:event.target.value})
+    }
+    const readValue=()=>{
+        console.log(data)
+    }
   return (
     <div>
         <NavBar/>
@@ -11,9 +21,9 @@ const SearchBook = () => {
                     <div className="row">
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">title</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control"name='title' value={data.title} onChange={inputHandler} />
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xl-6 col-xxl-6">
-                            <button className="btn btn-success">search</button>
+                            <button className="btn btn-success" onClick={readValue}>search</button>
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xl-6 col-xxl-6"></div>
                     </div>
